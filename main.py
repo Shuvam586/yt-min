d32d2d12f7f9a0e0e597f4468d43904e1d016705
Shuvam586 <shanta.mitra.sm@gmail.com>
2023-12-07 20:19:36 +0530

    chotto change

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def view(ytid):
     return render_template('view.html', vid=ytid, title=lookupVID(ytid)[0]['title'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
